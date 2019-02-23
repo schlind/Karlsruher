559b193fd911fb1ac64342856389c0abd05088ae
Sascha Schlindwein <code@schlind.org>
2019-02-23 12:26:52 +0100

    Moved logging config into main()

--- karlsruher/__main__.py
+++ karlsruher/__main__.py
@@ -10,16 +10,18 @@ import logging
 import sys
 from karlsruher import CommandLine
 
-logging.basicConfig(
-    level=logging.DEBUG if '-debug' in sys.argv else logging.INFO,
-    format='%(asctime)s %(levelname)-5.5s [%(name)s.%(funcName)s]: %(message)s',
-    handlers=[logging.StreamHandler()]
-)
-
 
 def main():
-    '''__main__.main()'''
+    '''karlsruher.__main__.main()'''
+
+    logging.basicConfig(
+        level=logging.DEBUG if '-debug' in sys.argv else logging.INFO,
+        format='%(asctime)s %(levelname)-5.5s [%(name)s.%(funcName)s]: %(message)s',
+        handlers=[logging.StreamHandler()]
+    )
+
     exit(CommandLine.run())
+
 
 if __name__ == '__main__':
     main()
